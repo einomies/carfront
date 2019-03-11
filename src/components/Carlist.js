5e559dc56dd10aad13cc5ca8a1853014ba89630e
einomies26 <einokamarainen@hotmail.com>
2019-03-11 23:04:29 +0200

    react-confirm-alert + delete functionality

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { SERVER_URL } from '../constants.js'
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 class Carlist extends Component {
 
@@ -10,7 +12,17 @@ class Carlist extends Component {
         this.state = { cars: [] };
     }
 
+    // We take the fetchCars function out from the componentDidMount() method. That
+    // is needed because we want to call the fetchCars function also after the car 
+    // has been deleted to show an updated list of the cars to the user. Create a 
+    // new function, called fetchCars(), and copy the code from the componentDidMount()
+    // method into a new function. Then call the fetchCars() function from the
+    // componentDidMount() function to fetch cars initially.
     componentDidMount() {
+        this.fetchCars();
+    }
+
+    fetchCars() {
         fetch(SERVER_URL + 'api/cars')
             .then((response) => response.json())
             .then((responseData) => {
@@ -19,6 +31,32 @@ class Carlist extends Component {
                 });
             })
             .catch(err => console.error(err));
+    }
+
+    // We send the DELETE request to a car link, and when the delete succeeds,
+    // we refresh the list page by calling the fetchCars() function.
+    onDelClick = (link) => {
+        console.log('delete car: ' + link)
+        fetch(link, { method: 'DELETE' })
+            .then(res => this.fetchCars())
+            .catch(err => console.error(err))
+    }
+
+    confirmDelete = (link) => {
+        confirmAlert(
+            {
+                message: 'Are you sure to delete?',
+                buttons: [
+                    {
+                        label: 'Yes',
+                        onClick: () => this.onDelClick(link)
+                    },
+                    {
+                        label: 'No',
+                    }
+                ]
+            }
+        )
     }
 
     render() {
@@ -47,6 +85,16 @@ class Carlist extends Component {
                 Header: 'Price €',
                 accessor: 'price'
             },
+            {
+                id: 'delbutton',
+                sortable: false,
+                filterable: false,
+                width: 100,
+                accessor: '_links.self.href',
+                Cell: (
+                    { value }) => (<button onClick={() => { this.confirmDelete(value) }}>Delete</button>
+                    )
+            }
         ]
         return (
             <div className="App">
