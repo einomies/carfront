36974e2d0410e5657c22ab089254d0ac622de1a5
einomies26 <einokamarainen@hotmail.com>
2019-03-17 11:05:05 +0200

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Carlist from './components/Carlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">CarList</h1>
-        </header>
+        <AppBar position="static" color="default">
+          <Toolbar>CarList</Toolbar>
+        </AppBar>
         <Carlist />
       </div >
     );
