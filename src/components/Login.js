b0f0d1ed8772f955aea61e2b55bf5d4ce0951a31
einomies26 <einokamarainen@hotmail.com>
2019-04-07 20:18:24 +0300

    Login.js v0.2

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { SERVER_URL } from '../constants.js';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Carlist from './Carlist';
 
 class Login extends Component {
 
@@ -16,6 +17,32 @@ class Login extends Component {
 
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
+    }
+
+    // Login is done by calling the /login endpoint using the POST method and sending
+    // the user object inside the body. If authentication succeeds, we get a token in a
+    //  response Authorization header. We will then save the token to session storage 
+    // and set the isAuthenticated state value to true. The session storage is similar 
+    // to local storage but it is cleared when a page session ends. When the isAuthenticated
+    // state value is changed, the user interface is re-rendered.
+    login = () => {
+        const user = { username: this.state.username, password: this.state.password };
+        fetch(
+            SERVER_URL + 'login', {
+                method: 'POST',
+                body: JSON.stringify(user)
+            }
+        )
+            .then(res => {
+                const jwtToken = res.headers.get('Authorization');
+                if (jwtToken !== null) {
+                    sessionStorage.setItem("jwt", jwtToken);
+                    this.setState({ isAuthenticated: true });
+                }
+            })
+            .catch(err => {
+                console.error(err)
+            })
     }
 
     render() {
